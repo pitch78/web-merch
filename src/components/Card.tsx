70a0bc9093b45f875d7d833b5e10babfa1069fec
François-Pierre PASQUIER <pitch78@gmail.com>
2025-03-15 16:24:22 +0100

    first style and component

---
+++ src/components/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+        <div>
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus commodi in itaque, laborum nobis repudiandae?</p>
+        </div>
+    )
+}
